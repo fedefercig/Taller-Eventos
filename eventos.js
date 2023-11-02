d11e43c72c39349228d2f9d20e7993c29edd9f93
fedefercig <141590152+fedefercig@users.noreply.github.com>
2023-11-02 04:47:09 -0300

    Alerts
    Leve modificación de los textos de los alert.

--- eventos.js
+++ eventos.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     let btnDiv = document.getElementById("div");
     btnDiv.addEventListener('click', function() {
-        alert("Hola soy el div!");
+        alert("¡Hola! ¡Soy el div!");
     });
 });
