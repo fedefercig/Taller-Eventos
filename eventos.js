c39daf8d57bf58dff3ceb64d3a1da292c53d66fe
fedefercig <141590152+fedefercig@users.noreply.github.com>
2023-11-02 04:43:28 -0300

    "Hola soy el div!"
    Creación del div en el HTML
    Creación de estilos.css
    Creación de eventos.js

---
+++ eventos.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let btnDiv = document.getElementById("div");
+    btnDiv.addEventListener('click', function() {
+        alert("Hola soy el div!");
+    });
+});
