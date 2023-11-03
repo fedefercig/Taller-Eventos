d651b540d08ecceb8399e2e6f8998f10a865cd34
fedefercig <141590152+fedefercig@users.noreply.github.com>
2023-11-02 22:03:25 -0300

    Reorganización Carpetas

--- Semana 7/Taller/eventos.js
+++ Semana 7/Taller/eventos.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let btnDiv = document.getElementById("div");
+    const btnDiv = document.getElementById("div");
     btnDiv.addEventListener('click', function() {
         alert("¡Hola! ¡Soy el div!");
     });
